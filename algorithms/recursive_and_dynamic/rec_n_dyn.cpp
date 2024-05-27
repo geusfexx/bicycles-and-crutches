d8aa9c2396eefd096d3fb549f71541b817b51bae
geusfexx <geusfexx@yandex.ru>
2024-05-27 10:12:29 -0700

    recursive_and_dynamic Initial commit. Chess king ways combinations algoryth was added.

---
+++ algorithms/recursive_and_dynamic/rec_n_dyn.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include "rec_n_dyn.h"
+
+//finds paths amount from 1, 1 to pos_x, pos_y coordinates
+//chess_king can move right and down only
+uint64_t chess_king_rec(uint64_t pos_x, uint64_t pos_y)
+{
+	//Base Case
+	if (pos_x == 1 || pos_y == 1) {
+		return 1;
+	}
+
+	return chess_king_rec(pos_x, pos_y -1) + chess_king_rec(pos_x -1, pos_y);
+}
+
+//finds paths amount from 1, 1 to pos_x, pos_y coordinates
+//chess_king can move right and down only
+uint64_t chess_king_dyn(uint64_t pos_x, uint64_t pos_y)
+{
+
+	std::vector<std::vector<uint64_t> > ways(pos_y + 1, (std::vector<uint64_t>(pos_x + 1)));
+
+	//Base Cases
+
+	for (size_t i(0); i <= pos_x; ++i)
+	{
+		ways[1][i] = 1;
+	}
+
+	for (size_t i(0); i <= pos_y; ++i)
+	{
+		ways[i][1] = 1;
+	}
+
+	//Conquered cases
+	for (size_t i(2); i <= pos_y; ++i)
+	{
+		for (size_t j(2); j <= pos_x; ++j)
+		{
+			ways[i][j] = ways[i][ j - 1] + ways[i - 1][j];
+		}
+	}
+
+	return ways[pos_y][pos_x];
+}
+
+int main()
+{
+
+	std::cout << chess_king_rec(10,15) << '\n';
+	std::cout << chess_king_dyn(10,15) << '\n';
+
+    return 0;
+}
---
+++ algorithms/recursive_and_dynamic/rec_n_dyn.h
@@ -0,0 +1,2 @@
+uint64_t chess_king_rec(uint64_t pos_x, uint64_t pos_y);
+uint64_t chess_king_dyn(uint64_t pos_x, uint64_t pos_y);
