fe6c308a14114635b8d604df335bea17f95a6f7d
geusfexx <geusfexx@yandex.ru>
2024-05-28 11:06:46 -0700

    levenshtein_distance function was added.

--- algorithms/recursive_and_dynamic/rec_n_dyn.h
+++ algorithms/recursive_and_dynamic/rec_n_dyn.h
@@ -5,3 +5,4 @@ uint64_t chess_king_rec(uint64_t pos_x, uint64_t pos_y);
 uint64_t chess_king_dyn(uint64_t pos_x, uint64_t pos_y);
 
 uint32_t knapsack_solve(std::vector<std::pair<uint32_t, uint32_t> > items, const uint32_t capacity);
+uint32_t levenshtein_distance(const std::string a, const std::string b);
