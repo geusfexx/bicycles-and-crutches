03206e6ba93bc7fab25bcf9bb41d189deee8cb46
geusfexx <geusfexx@yandex.ru>
2024-05-29 09:16:06 -0700

    Tests template was added.

--- algorithms/strings/strings.cpp
+++ algorithms/strings/strings.cpp
@@ -1,1 +1,8 @@
 #include "strings.h"
+
+std::vector<uint32_t> kmp(const std::string pattern, const std::string line)
+{
+	std::vector<uint32_t> result = {};
+
+	return result;
+}
---
+++ algorithms/strings/strings.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<uint32_t> kmp(const std::string pattern, const std::string line);
