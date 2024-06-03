a5416b8d7ab528251e7b9bf37a86b255e64f641b
geusfexx <geusfexx@yandex.ru>
2024-06-03 10:00:36 -0700

    graph - initial commit.

---
+++ algorithms/graphs/graph_example.cpp
@@ -0,0 +1,11 @@
+#include <map>
+#include <set>
+
+typedef int32_t vertex_t;
+typedef std::set<vertex_t> vertexes;
+typedef std::set<std::pair<vertex_t, vertex_t> > edges;
+
+int main()
+{
+	return 0;
+}
