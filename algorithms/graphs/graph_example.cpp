a411a7c658291986fdf57d6eaeea56a08e638918
geusfexx <geusfexx@yandex.ru>
2024-06-04 08:15:59 -0700

    GraphTypeSimple has been implemented.

--- algorithms/graphs/graph_example.cpp
+++ algorithms/graphs/graph_example.cpp
@@ -1,42 +1,96 @@
 #include <map>
 #include <set>
+#include <vector>
 #include <iostream>
 
 
-class GraphType
+class GraphTypeSimple
 {
 public:
 typedef int32_t Vertex;
 typedef std::set<Vertex> Vertexes;
 typedef std::set<std::pair<Vertex, Vertex> > EdgesSet;
 
-	GraphType(){}
-	GraphType(const int32_t& vertexes_amount, const EdgesSet& edges) :
+    class GraphTypeSimple_exception: public std::exception
+    {
+    public:
+        GraphTypeSimple_exception(const std::string& message): iMessage(message)
+        {}
+        const char* what() const noexcept override
+        {
+            return iMessage.c_str();
+        }
+    private:
+        std::string iMessage;
+    };
+
+        GraphTypeSimple(){}
+        GraphTypeSimple(const int32_t& vertexes_amount, const EdgesSet& edges) :
 			 iVertexesAmount(vertexes_amount), iEdges(edges){}
 
-	void fill_edges(const int32_t& vertexes_amount, const EdgesSet& edges) {}
+        void fill_edges(const int32_t& vertexes_amount, const EdgesSet& edges)
+        {
+            if (vertexes_amount == 0 and edges.empty() or
+                                        vertexes_amount != 0){
+                iEdges = edges;
+                iVertexesAmount = vertexes_amount;
+                return;
+            }
+
+            throw GraphTypeSimple_exception("Impossible to create any edges without vertexes!");
+        }
+
 	void print_edges() const
 	{
 		std::cout << "Vertexes amount: " << iVertexesAmount << "\n{";
-		for (auto edge : iEdges)
+                for (const auto& edge : iEdges)
 		{
 			std::cout << " {" << edge.first << ", " << edge.second << "}";
 		}
 		std::cout << " }\n";
 	}
 
-public:
-	int32_t iVertexesAmount;
-	EdgesSet iEdges;
+        void clean()
+        {
+            iVertexesAmount = 0;
+            iEdges.clear();
+        }
 
+public:
+        int32_t iVertexesAmount = 0;
+        EdgesSet iEdges = {};
 };
+
+void test()
+{
+    std::vector<std::pair<int32_t, GraphTypeSimple::EdgesSet>> subjects = {
+        {4, {{1,3},{4,1},{2,3},{2,4},{3,4}}},
+        {3, {{1,3},{2,3},{3,1},{2,1}}},
+        {3, {}},
+        {0, {}},
+        {0, {{1,3},{2,3},{3,1},{2,1}}},
+        {0, {{}}}
+    };
+
+    GraphTypeSimple g;
+
+    for (auto params: subjects)
+    {
+        try {
+            g.fill_edges(params.first, params.second);
+            g.print_edges();
+            g.clean();
+        } catch (const GraphTypeSimple::GraphTypeSimple_exception& ex) {
+            std::cout << ex.what() << "\n";
+        } catch (const std::exception&) {
+            std::cout << "Something else wrong"<< std::endl;
+        }
+    }
+}
 
 int main()
 {
-	GraphType::EdgesSet eges = {{1,3},{4,1},{2,3},{2,4},{3,4}};
-	GraphType g(4, eges);
-	
-	g.print_edges();
+    test();
 
-	return 0;
+    return 0;
 }
