db45ba00d8aa973e2bc644e2c0ae1458e0b7313e
geusfexx <geusfexx@yandex.ru>
2024-06-06 09:46:04 -0700

    AdjacencyMapType class was has been added. Print impl was refactored. DFS was implemented (recursive).

--- algorithms/graphs/test.h
+++ algorithms/graphs/test.h
@@ -1,10 +1,6 @@
 #ifndef test_h
 #define test_h
 
-#include <map>
-#include <set>
-#include <vector>
-#include <iostream> // WTF? it causes "still reachable: 72,704 bytes in 1 blocks"
-#include "graph_example.cpp"
+void test();
 
 #endif // test_h
