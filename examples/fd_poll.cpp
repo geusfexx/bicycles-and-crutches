97cb9f52f7b0629fe65627ceed600e12d05e6d4d
geusfexx <geusfexx@yandex.ru>
2025-02-01 13:51:30 -0800

    sockets - fd_poll loop has been added.

--- examples/fd_poll.cpp
+++ examples/fd_poll.cpp
@@ -7,22 +7,27 @@
 int main()
 {
     struct pollfd fds[2];
-
+    const char* quit = "FLUGGAENKDECHIOEBOLSEN";
     fds[0].fd = 0;
     fds[0].events = POLLIN;
 
-    int ret = poll(fds, 1, TIMEOUT*1000);
+    while(true) {
+        int ret = poll(fds, 1, TIMEOUT*1000);
 
-    if (ret) {
-        printf("Data is ready\n");
-
-        if (fds[0].revents & POLLIN){
-            char buf[1024];
-            scanf("%s", buf);
-            printf("%s\n", buf);
+        if (ret) {
+            if (fds[0].revents & POLLIN){
+                char buf[1024];
+                scanf("%s", buf);
+                if (*buf == *quit) {
+                    printf("Exit code has been received\n");
+                    break;
+                }
+                printf("Data is ready\n");
+                printf("%s\n", buf);
+            }
+        } else {
+            printf("TIMEOUT\n");
         }
-    } else {
-        printf("TIMEOUT\n");
     }
 
     return 0;
